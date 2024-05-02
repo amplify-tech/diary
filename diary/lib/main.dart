e05f08a665cd78e6e3d2a85f67f92485d0860b89
Akram Khan <59254070+amplify-tech@users.noreply.github.com>
2024-05-02 17:40:38 +0530

    setting:github no reply email added

--- diary/lib/main.dart
+++ diary/lib/main.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Contact Diary',
+      title: 'Contact Diary App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         useMaterial3: true,
