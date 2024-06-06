b0ccfe4e42114f3859e60fb65f8afd7df7b282ce
Akram Khan <59254070+amplify-tech@users.noreply.github.com>
2024-06-07 00:37:52 +0530

    firebase configuration added

--- diary/lib/main.dart
+++ diary/lib/main.dart
@@ -3,10 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:diary/widgets/common/contact_navigation.dart';
 import 'package:provider/provider.dart';
 import 'package:diary/data/repositories/isar_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await IsarService.initialize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) => TagProvider()),
