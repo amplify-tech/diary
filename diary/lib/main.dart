69ad5b4f10a2fb499af53d21f65d49b97baecfed
Akram Khan <59254070+amplify-tech@users.noreply.github.com>
2024-06-15 02:11:13 +0530

    firebase notification working

--- diary/lib/main.dart
+++ diary/lib/main.dart
@@ -1,27 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  print('Got a message whilst in the background!');
-  print('Message: ${message.notification?.title}');
-}
-
-void main() async {
+var kDebugMode = true;
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  FirebaseMessaging.instance.getToken().then((token) {
-    print('Res Token: $token');
-  });
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  if (kDebugMode) {
+    print('Permission granted: ${settings.authorizationStatus}');
+  }
+
+  String? token = await messaging.getToken();
+  if (kDebugMode) {
+    print('Registration Token=$token');
+  }
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print('Got a message whilst in the foreground!');
-    print('Message: ${message.notification?.title}');
-  });
+    if (kDebugMode) {
+      print('Handling a foreground message: ${message.messageId}');
+      print('Message data: ${message.data}');
+      print('Message notification: ${message.notification?.title}');
+      print('Message notification: ${message.notification?.body}');
+    }
 
-  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-    print('Message clicked!');
+    _showNotification(message);
   });
 
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
@@ -29,17 +47,51 @@ void main() async {
   runApp(const MyApp());
 }
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  if (message.notification != null) {
+    print('Message notification: ${message.notification?.title}');
+    print('Message notification: ${message.notification?.body}');
+  }
+
+  print('Message data: ${message.data}');
+
+  _showNotification(message);
+}
+
+void _showNotification(RemoteMessage message) {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  AndroidInitializationSettings androidSettings =
+      const AndroidInitializationSettings('com.example.diary');
+  InitializationSettings initializationSettings =
+      InitializationSettings(android: androidSettings);
+
+  flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  AndroidNotificationDetails androidNotificationDetails =
+      const AndroidNotificationDetails('channelId', 'channelName',
+          importance: Importance.max, priority: Priority.high);
+  NotificationDetails notificationDetails =
+      NotificationDetails(android: androidNotificationDetails);
+
+  flutterLocalNotificationsPlugin.show(
+      message.notification?.title.hashCode ?? 0,
+      message.notification?.title,
+      message.notification?.body,
+      notificationDetails);
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+    return const MaterialApp(
+      title: 'My App',
+      home: MyHomePage(),
     );
   }
 }
@@ -58,16 +110,14 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Firebase Messaging Demo'),
+        title: const Text('My App'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Last Message:',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
+            Text('Last message from Firebase Messaging:',
+                style: Theme.of(context).textTheme.titleLarge),
             Text(_lastMessage, style: Theme.of(context).textTheme.bodyLarge),
           ],
         ),
